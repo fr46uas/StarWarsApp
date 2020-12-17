b83173e06cd0fc6a28f6100fa4fcf6e651017304
fr46uas <sergio.fraguas@gmail.com>
2020-12-17 20:04:39 +0100

    Creacion del navbar +footer + home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilmsComponent } from './pages/films/films.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { SpaceshipsComponent } from './pages/spaceships/spaceships.component';
+import { SpeciesComponent } from './pages/species/species.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'planets', component: PlanetsComponent },
+  { path: 'species', component: SpeciesComponent },
+  { path: 'spaceship', component: SpaceshipsComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: 'films', component: FilmsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { SpaceshipsComponent } from './pages/spaceships/spaceships.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
+import { PeopleComponent } from './pages/people/people.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { SpeciesComponent } from './pages/species/species.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    SpaceshipsComponent,
+    VehiclesComponent,
+    PeopleComponent,
+    FilmsComponent,
+    SpeciesComponent,
+    NavbarComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
